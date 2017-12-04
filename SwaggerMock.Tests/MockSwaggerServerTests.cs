04230212b4d12ea6417d91d4fa48aa7b78d8ae43
Decads <declancook44@gmail.com>
2017-12-04 01:14:49 +0000

    Start working on validating requests

--- SwaggerMock.Tests/MockSwaggerServerTests.cs
+++ SwaggerMock.Tests/MockSwaggerServerTests.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
 
 namespace SwaggerMock.Tests
 {
@@ -19,45 +21,6 @@ namespace SwaggerMock.Tests
         }
 
         [TestMethod]
-        public async Task Should_Handle_GET_Requests()
-        {
-            var res = await _mockServer.Client.GetAsync("/api/pets/");
-            Assert.IsTrue(res.IsSuccessStatusCode);
-        }
-
-        [TestMethod]
-        public async Task Should_Handle_GET_Requests_With_Params()
-        {
-            var res = await _mockServer.Client.GetAsync("/api/pets/4");
-            Assert.IsTrue(res.IsSuccessStatusCode);
-        }
-
-       /// <summary>
-       /// asdasd
-       /// </summary>
-        [TestMethod]
-        public async Task Should_Handle_GET_Requests_With_Params_And_Query()
-        {
-            var res = await _mockServer.Client.GetAsync("/api/pets/4?tags=goat,duck");
-            Assert.IsTrue(res.IsSuccessStatusCode);
-        }
-
-        [TestMethod]
-        public async Task Should_Handle_POST_Requests()
-        {
-            var res = await _mockServer.Client.PostAsync("/api/pets/", new StringContent(""));
-            Assert.IsTrue(res.IsSuccessStatusCode);
-        }
-
-        // Todo Add PUT to JSON
-        // [TestMethod]
-        // public async Task Should_Handle_PUT_Requests()
-        // {
-        //    var res = await _mockServer.Client.PutAsync("/api/pets/1", new StringContent(""));
-        //    Assert.IsTrue(res.IsSuccessStatusCode);
-        // }
-
-        [TestMethod]
         public async Task Should_Error_On_Unregistered_Routes()
         {
             var res = await _mockServer.Client.GetAsync("/api/goats");
@@ -65,10 +28,49 @@ namespace SwaggerMock.Tests
         }
 
         [TestMethod]
-        public async Task Should_Validate_Get_Parameters()
+        public async Task Should_Validate_Required_Path_Parameters()
+        {
+            var res = await _mockServer.Client.GetAsync("/api/pets/1");
+            Assert.IsTrue(res.IsSuccessStatusCode);
+        }
+
+        [TestMethod]
+        public async Task Should_Validate_Path_Parameters_Types()
         {
             var res = await _mockServer.Client.GetAsync("/api/pets/goat");
+            var response = await res.Content.ReadAsStringAsync();
+            var errors = JsonConvert.DeserializeObject<List<SwaggerParameterError>>(response);
+
             Assert.IsFalse(res.IsSuccessStatusCode);
+            Assert.IsTrue(errors.Count == 1);
+            Assert.AreEqual("Invalid parameter type. Expected id to be Integer", errors[0].Message);
+
+            var validRes = await _mockServer.Client.GetAsync("/api/pets/1");
+            Assert.IsTrue(validRes.IsSuccessStatusCode);
+        }
+
+        [TestMethod]
+        public async Task Should_Validate_Required_Body_Parameters()
+        {
+            var res = await _mockServer.Client.PostAsync("/api/pets/", new StringContent(string.Empty));
+            var response = await res.Content.ReadAsStringAsync();
+            var errors = JsonConvert.DeserializeObject<List<SwaggerParameterError>>(response);
+
+            Assert.IsFalse(res.IsSuccessStatusCode);
+            Assert.IsTrue(errors.Count == 1);
+            Assert.AreEqual("Missing required parameter pet", errors[0].Message);
+        }
+
+        [TestMethod]
+        public async Task Should_Validate_Required_Body_Parameters_And_Properties()
+        {
+            var res = await _mockServer.Client.PostAsync("/api/pets/", new StringContent(@"{ ""invalid"": ""prop""}"));
+            var response = await res.Content.ReadAsStringAsync();
+            var errors = JsonConvert.DeserializeObject<List<SwaggerParameterError>>(response);
+
+            Assert.IsFalse(res.IsSuccessStatusCode);
+            Assert.IsTrue(errors.Count == 1);
+            Assert.AreEqual("PropertyRequired: #/name", errors[0].Message);
         }
     }
 }
--- SwaggerMock/RouteValidatorFactory.cs
+++ SwaggerMock/RouteValidatorFactory.cs
@@ -1,5 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Newtonsoft.Json;
+using NJsonSchema;
 using NSwag;
 
 namespace SwaggerMock
@@ -8,7 +14,126 @@ namespace SwaggerMock
     {
         public RequestDelegate GetValidatorDelegate(SwaggerOperationMethod method, SwaggerOperation operation)
         {
-            return context => context.Response.WriteAsync("Ok");
+            var parameterValidator = new SwaggerValidator();
+            return context =>
+            {
+                var errors = parameterValidator.Validate(context, operation);
+
+                if (errors.Any())
+                {
+                    context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                    return context.Response.WriteAsync(JsonConvert.SerializeObject(errors));
+                }
+
+                return context.Response.WriteAsync(@"{ ""status"": ""ok""}");
+            };
         }
+    }
+
+    public class SwaggerValidator
+    {
+        public List<SwaggerParameterError> Validate(HttpContext context, SwaggerOperation operation)
+        {
+            var errors = new List<SwaggerParameterError>();
+            foreach (var param in operation.Parameters)
+            {
+                if (param.Kind == SwaggerParameterKind.Path)
+                {
+                    var value = context.GetRouteValue(param.Name);
+                    if (value == null && param.IsRequired)
+                    {
+                        errors.Add(new SwaggerParameterError
+                        {
+                            Message = $"Missing Required Parameter {param.Name}",
+                        });
+                    }
+
+                    if (param.IsRequired && !ValidType(param.Type, value))
+                    {
+                        errors.Add(new SwaggerParameterError
+                        {
+                            Message = $"Invalid parameter type. Expected {param.Name} to be {param.Type}",
+                        });
+                    }
+                }
+
+                if(param.Kind == SwaggerParameterKind.Body)
+                {
+                    //TODO check content type and swagger def
+                    var body = ReadBody(context.Request.Body);
+                    if (param.IsRequired && string.IsNullOrEmpty(body))
+                    {
+                        errors.Add(new SwaggerParameterError
+                        {
+                            Message = $"Missing required parameter {param.Name}",
+                        });
+                    }
+
+                    if (!string.IsNullOrEmpty(body))
+                    {
+                        var validationErrors = param.Schema.Validate(body);
+                        if (validationErrors.Any())
+                        {
+                            errors.AddRange(validationErrors.Select(x => new SwaggerParameterError
+                            {
+                                Message = x.ToString(),
+                            }));
+                        }
+                    }
+                }
+            }
+
+            return errors;
+        }
+
+        private bool ValidType(JsonObjectType paramType, object value)
+        {
+            try
+            {
+                switch (paramType)
+                {
+                    case JsonObjectType.None:
+                        break;
+                    case JsonObjectType.Array:
+                        break;
+                    case JsonObjectType.Boolean:
+                        break;
+                    case JsonObjectType.Integer:
+                        var val = Convert.ToInt32(value);
+                        return true;
+                    case JsonObjectType.Null:
+                        break;
+                    case JsonObjectType.Number:
+                        break;
+                    case JsonObjectType.Object:
+                        break;
+                    case JsonObjectType.String:
+                        break;
+                    case JsonObjectType.File:
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException(nameof(paramType), paramType, null);
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            return false;
+        }
+
+        private string ReadBody(Stream stream)
+        {
+            using (var sr = new StreamReader(stream))
+            {
+                return sr.ReadToEnd();
+            }
+        }
+    }
+
+    public class SwaggerParameterError
+    {
+        public string Message { get; set; }
     }
 }
